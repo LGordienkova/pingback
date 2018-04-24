17dbc8e19ebdba18294a67a8c6127fcb88a7a49e
Lada Gordienkova <l.gordienkova@gmail.com>
2018-04-24 15:45:55 +0300

    Done

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const data = [{}];
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+
+app
+  .use(express.json())
+  .post('/q', (req, res) => {
+    const { body } = req;
+
+    data.push(body);
+
+    console.log('Received new data:');
+    console.log(body);
+
+    io.emit('new data', body);
+
+    res.status(201).json({ status: 'created' });
+  })
+  .get('/q', (req, res) => {
+    res.json({ data });
+  })
+  .get('/q/latest', (req, res) => {
+    res.json(data[data.length - 1]);
+  })
+  .use(express.static('./public'))
+
+server.listen(1337, () => { console.log('Listening on :1337'); })
